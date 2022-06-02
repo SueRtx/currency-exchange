3c8830b30eaa1d0e71233f8fb9a42d3a0086d873
Sue Roberts <suerobertstx@yahoo.com>
2022-06-02 14:42:03 -0500

    Add exchange.js

--- src/exchange.js
+++ src/exchange.js
@@ -1,14 +1,16 @@
-/*export default class WeatherService {  
-  static async getWeather(city) {
-    try {
-      const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`);
+export default class CurrencyExchange {  
+  static getExchange(country, dollar) {
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${country}/${dollar}`)
+    .then(function(response) {
+
       if (!response.ok) {
         throw Error(response.statusText);
       }
       return response.json();
-    } catch(error) {
+    })
+    .catch(function(error) {
       return error.message;
-    }
+    });
+
   }
 }
-*/
