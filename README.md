decbc10db9f7fb84420fa6b4cae2072c07bcfbdc
Sue Roberts <99991896+SueRtx@users.noreply.github.com>
2022-06-02 13:19:14 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Title
+
+#### By Sue Roberts
+
+#### Description
+
+## Technologies Used
+*
+* 
+* 
+* 
+* 
+
+## Description 
+
+
+
+## Setup/Installation Requirements
+
+* 
+* 
+* 
+
+## Known Bugs
+
+* None
+
+## License
+
+MIT
+
+Copyright (c) 2022 Sue Roberts
---
+++ src/Index.js
@@ -0,0 +1,38 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+//import WeatherService from './weather-service.js';
+
+
+
+/*
+function clearFields() {
+  $('#location').val("");
+  $('.showErrors').text("");
+  $('.showHumidity').text("");
+  $('.showTemp').text("");
+}
+
+function getElements(response) {
+  if (response.main) {
+    $('.showHumidity').text(`The humidity in ${response.name} is ${response.main.humidity}%`);
+    $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+  } else {
+    $('.showErrors').text(`There was an error: ${response}`);
+  }
+}
+
+async function makeApiCall(city) {
+  const response = await WeatherService.getWeather(city);
+  getElements(response);
+}
+
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    let city = $('#location').val();
+    clearFields();
+    makeApiCall(city);
+  });
+});
+*/
